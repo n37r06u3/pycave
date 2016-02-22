68e5514daae00c8529e3b049916a698807c1f4ef
n37r06u3 <n37r06u3@gmail.com>
2016-02-22 23:19:29 +0800

    update

--- pycave/apps/blog/urls.py
+++ pycave/apps/blog/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from .views import BlogPostDetailView, BlogPostListView
 
 urlpatterns = [
-    url('^$', BlogPostListView.as_view(), name='blog'),
+    url('^$', BlogPostListView.as_view(), name='home'),
     # url('^article/(?P<slug>[a-z0-9-]+)/$',
     #     BlogPostDetailView.as_view(), name='blogpost-detail'),
     url('^post/(?P<pk>\d+)/$',
