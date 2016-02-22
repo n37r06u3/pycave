e253e2ca0ad435f2ea4210f656edada8d027e373
n37r06u3 <n37r06u3@gmail.com>
2016-02-22 23:15:26 +0800

    update

--- README.md
+++ README.md
@@ -6,3 +6,9 @@ pycave.com django project
 # 部署流程
 apt-get update
 apt-get install nginx -y
+
+
+
+# 参考
+http://fontawesome.io/icons/
+https://github.com/zniper/simple-article
--- pycave/apps/blog/templatetags/article_tags.py
+++ pycave/apps/blog/templatetags/article_tags.py
@@ -8,7 +8,7 @@ register = template.Library()
 @register.assignment_tag
 def recent_articles(limit=10, exclude=None):
     """Returns list of latest article"""
-    queryset = BlogPost.objects.order_by('-created')
+    queryset = BlogPost.objects.order_by('-created')[:limit]
     if exclude:
         if hasattr(exclude, '__iter__'):
             queryset = queryset.exclude(pk__in=exclude)
